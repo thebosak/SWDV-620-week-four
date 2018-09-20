e0f946e772985a0e0c603330bc952516182ff7ca
bbosak1 <33043855+bbosak1@users.noreply.github.com>
2018-09-20 12:32:34 -0500

    commit

--- user_prompt.js
+++ user_prompt.js
@@ -1,4 +1,4 @@
-function user_prompt () {
+function userPrompt () {
     //declare variables    
     var username;
     var pwd;
@@ -9,11 +9,15 @@ function user_prompt () {
     username=prompt("Username: ", "enter username in this box");
     pwd=prompt("Password: ", " ");
 
+    verifyUser(username,pwd, user1, user2);
+}
+
+function verifyUser(username, pwd, user1, user2) {
+
     if (username == user1 || username == user2) {
         alert("Welcome, " + username + "!");
         alert("Here's your pwd: " + pwd);
     } else {
         alert("Sorry, you are not a recognized user.");
     }
-
 }
